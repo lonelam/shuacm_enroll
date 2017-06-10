af01080d29abc961950092ed6bb21772ceb641e1
lonelam <laizenan@gmail.com>
2017-06-10 13:18:31 +0800

    make static works

--- shuacm/settings.py
+++ shuacm/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://acm.lonelam.me/static/'
 STATIC_ROOT = 'static'
